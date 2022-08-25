a69f17542ce26852e0db5ff0515e7d2c0d5c0cba
Bruno Werner <bruno_werner@estudante.sesisenai.org.br>
2022-08-24 22:34:50 -0300

    comecei o projeto novo "login"

---
+++ one piece/js/script.js
@@ -0,0 +1,8 @@
+function mostrarOcultarSenha () {
+    var senha=document.getElementById("senha");
+    if(senha.type=="password"){
+        senha.type="text"
+    }else{
+        senha.type="password" 
+    }
+}
